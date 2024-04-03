4bafc7aca783e9eb02f0e1c756c96c7154f2d8fe
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-04-03 07:14:08 +0000

    Improve build - adding new feature

--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -20,4 +20,4 @@ export function getWallet(wallet: string): Keypair {
   // most likely someone pasted base58 encoded private key
   return Keypair.fromSecretKey(bs58.decode(wallet));
 }
-Change auth - removing deprecated function
+Change auth - removing deprecated functionImprove build - adding new feature
