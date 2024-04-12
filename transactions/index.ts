4b5d7d16f49d7eda500a577a806871d8e8972d8f
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-04-12 04:08:49 +0000

    Fix configuration - enhancing logs

--- transactions/index.ts
+++ transactions/index.ts
@@ -1,3 +1,3 @@
 export * from './default-transaction-executor';
 export * from './transaction-executor.interface';
-Remove README - adding new feature
+Remove README - adding new featureFix configuration - enhancing logs
