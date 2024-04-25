f499e9deb298ba3265b7efbbdd889d40748da50a
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-04-25 00:33:19 +0000

    Add dependencies - fixing a bug

--- helpers/promises.ts
+++ helpers/promises.ts
@@ -1,2 +1,2 @@
 export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
-Optimize build - refactoring code
+Optimize build - refactoring codeAdd dependencies - fixing a bug
