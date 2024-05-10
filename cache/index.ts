36225c8173430090044eb9dbb36537ccebcbefec
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-05-10 12:42:31 +0000

    Improve dependencies - fixing typo

--- cache/index.ts
+++ cache/index.ts
@@ -1,4 +1,4 @@
 export * from './market.cache';
 export * from './pool.cache';
 export * from './snipe-list.cache';
-Fix README - improving performanceChange API - fixing typoImprove auth - updating docs
+Fix README - improving performanceChange API - fixing typoImprove auth - updating docsImprove dependencies - fixing typo
