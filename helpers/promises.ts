a3a07ff47f7dc9b44b3f02514d9696c021a34d46
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-05-11 18:56:52 +0000

    Revert README - enhancing logs

--- helpers/promises.ts
+++ helpers/promises.ts
@@ -1,2 +1,2 @@
 export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
-Optimize build - refactoring codeAdd dependencies - fixing a bug
+Optimize build - refactoring codeAdd dependencies - fixing a bugRevert README - enhancing logs
