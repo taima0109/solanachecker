b88bc23f3a614f61c476bf1b3b214bc87122754a
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-07-16 07:07:54 +0000

    Improve styles - fixing a bug

--- listeners/index.ts
+++ listeners/index.ts
@@ -1,2 +1,2 @@
 export * from './listeners';
-Update README - fixing a bugRemove UI - handling edge casesAdd dependencies - fixing typo
+Update README - fixing a bugRemove UI - handling edge casesAdd dependencies - fixing typoImprove styles - fixing a bug
