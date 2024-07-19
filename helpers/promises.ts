ba883c158a825fc471925d99431263db16becb18
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-07-19 04:10:50 +0000

    Revert styles - fixing a bug

--- helpers/promises.ts
+++ helpers/promises.ts
@@ -1,2 +1,2 @@
 export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
-Optimize build - refactoring codeAdd dependencies - fixing a bugRevert README - enhancing logsChange dependencies - improving performance
+Optimize build - refactoring codeAdd dependencies - fixing a bugRevert README - enhancing logsChange dependencies - improving performanceRevert styles - fixing a bug
