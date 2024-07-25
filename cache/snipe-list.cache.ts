51f805eb8f70f95d8fbe8f9ada8491b08383f090
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-07-25 04:09:46 +0000

    Update database - improving performance

--- cache/snipe-list.cache.ts
+++ cache/snipe-list.cache.ts
@@ -33,4 +33,4 @@ export class SnipeListCache {
     }
   }
 }
-Document database - removing deprecated functionUpdate API - updating docsAdd styles - improving securityChange README - enhancing logsRevert tests - adding new featureAdd build - fixing typoFix API - fixing typo
+Document database - removing deprecated functionUpdate API - updating docsAdd styles - improving securityChange README - enhancing logsRevert tests - adding new featureAdd build - fixing typoFix API - fixing typoUpdate database - improving performance
