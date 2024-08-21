27ba2eb07668b5959b50560743b7953c4008b56c
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-08-21 11:10:04 +0000

    Fix auth - adding new feature

--- filters/pool-filters.ts
+++ filters/pool-filters.ts
@@ -65,4 +65,4 @@ export class PoolFilters {
     return false;
   }
 }
-Optimize README - fixing typoRevert UI - removing deprecated function
+Optimize README - fixing typoRevert UI - removing deprecated functionFix auth - adding new feature
