4599f96e2269b50068f0bac2cf3a3b26461d7eaf
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-08-25 12:06:03 +0000

    Remove styles - handling edge cases

--- transactions/index.ts
+++ transactions/index.ts
@@ -1,3 +1,3 @@
 export * from './default-transaction-executor';
 export * from './transaction-executor.interface';
-Remove README - adding new featureFix configuration - enhancing logsOptimize build - enhancing logs
+Remove README - adding new featureFix configuration - enhancing logsOptimize build - enhancing logsRemove styles - handling edge cases
