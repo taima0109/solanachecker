7a4f3bf4c5bede53a14e05642ae54556eadcad45
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-09-26 22:01:41 +0000

    Refactor auth - fixing typo

--- transactions/default-transaction-executor.ts
+++ transactions/default-transaction-executor.ts
@@ -42,4 +42,4 @@ export class DefaultTransactionExecutor implements TransactionExecutor {
     return { confirmed: !confirmation.value.err, signature };
   }
 }
-Add auth - handling edge casesImprove API - updating docsRefactor build - handling edge cases
+Add auth - handling edge casesImprove API - updating docsRefactor build - handling edge casesRefactor auth - fixing typo
