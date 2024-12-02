061f954ec89f891e87c82dab0bf16de21026f6ee
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-12-02 18:17:10 +0000

    Document configuration - refactoring code

--- listeners/index.ts
+++ listeners/index.ts
@@ -1,2 +1,2 @@
 export * from './listeners';
-Update README - fixing a bugRemove UI - handling edge casesAdd dependencies - fixing typoImprove styles - fixing a bugUpdate build - improving performanceUpdate API - updating docsFix API - refactoring codeChange README - enhancing logs
+Update README - fixing a bugRemove UI - handling edge casesAdd dependencies - fixing typoImprove styles - fixing a bugUpdate build - improving performanceUpdate API - updating docsFix API - refactoring codeChange README - enhancing logsDocument configuration - refactoring code
