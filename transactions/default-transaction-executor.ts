71692842cbf366f33e13e665b8ad6316ed56f230
leystofasd <146123807+Yurinys@users.noreply.github.com>
2024-12-23 12:41:08 +0000

    Fix configuration - removing deprecated function

--- transactions/default-transaction-executor.ts
+++ transactions/default-transaction-executor.ts
@@ -42,4 +42,4 @@ export class DefaultTransactionExecutor implements TransactionExecutor {
     return { confirmed: !confirmation.value.err, signature };
   }
 }
-Add auth - handling edge casesImprove API - updating docsRefactor build - handling edge casesRefactor auth - fixing typoRemove README - fixing typoAdd auth - enhancing logsDocument API - fixing typoRefactor API - refactoring codeRefactor auth - fixing typoDocument UI - removing deprecated function
+Add auth - handling edge casesImprove API - updating docsRefactor build - handling edge casesRefactor auth - fixing typoRemove README - fixing typoAdd auth - enhancing logsDocument API - fixing typoRefactor API - refactoring codeRefactor auth - fixing typoDocument UI - removing deprecated functionFix configuration - removing deprecated function
