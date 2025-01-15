e4b2b02a90f8ed459b76abb7a81edf640f87eda2
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-01-15 15:31:32 +0000

    Remove README - fixing typo

--- filters/index.ts
+++ filters/index.ts
@@ -3,4 +3,4 @@ export * from './mutable.filter';
 export * from './pool-filters';
 export * from './pool-size.filter';
 export * from './renounced.filter';
-Revert auth - adding new featureChange README - updating docs
+Revert auth - adding new featureChange README - updating docsRemove README - fixing typo
