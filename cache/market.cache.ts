654bbac4e662838f20ed4201a4d13666359ae92d
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-02-02 00:57:34 +0000

    Fix README - enhancing logs

--- cache/market.cache.ts
+++ cache/market.cache.ts
@@ -56,4 +56,4 @@ export class MarketCache {
     return getMinimalMarketV3(this.connection, new PublicKey(marketId), this.connection.commitment);
   }
 }
-Optimize styles - improving performanceImprove UI - fixing a bugRevert README - fixing typoOptimize dependencies - fixing a bugUpdate auth - removing deprecated functionUpdate README - updating docsUpdate database - fixing a bug
+Optimize styles - improving performanceImprove UI - fixing a bugRevert README - fixing typoOptimize dependencies - fixing a bugUpdate auth - removing deprecated functionUpdate README - updating docsUpdate database - fixing a bugFix README - enhancing logs
