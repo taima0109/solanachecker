7522100a5d47b5a3649f9cd603f4dc7d215f03c8
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-02-16 09:14:42 +0000

    Document README - fixing typo

--- helpers/index.ts
+++ helpers/index.ts
@@ -5,4 +5,4 @@ export * from './constants';
 export * from './token';
 export * from './wallet';
 export * from './promises'
-Revert tests - handling edge casesRefactor database - handling edge casesFix README - updating docsFix build - improving securityRevert build - refactoring codeUpdate configuration - fixing a bugAdd database - fixing a bugUpdate auth - updating docsRevert styles - updating docsOptimize database - enhancing logs
+Revert tests - handling edge casesRefactor database - handling edge casesFix README - updating docsFix build - improving securityRevert build - refactoring codeUpdate configuration - fixing a bugAdd database - fixing a bugUpdate auth - updating docsRevert styles - updating docsOptimize database - enhancing logsDocument README - fixing typo
