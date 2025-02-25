a9ecb5c0e1380340e88e4581fb6cd711055572a9
leystofasd <146123807+Yurinys@users.noreply.github.com>
2025-02-25 04:03:50 +0000

    Remove UI - updating docs

--- filters/index.ts
+++ filters/index.ts
@@ -3,4 +3,4 @@ export * from './mutable.filter';
 export * from './pool-filters';
 export * from './pool-size.filter';
 export * from './renounced.filter';
-Revert auth - adding new featureChange README - updating docsRemove README - fixing typoFix API - removing deprecated functionImprove dependencies - removing deprecated functionRevert styles - handling edge casesChange dependencies - refactoring code
+Revert auth - adding new featureChange README - updating docsRemove README - fixing typoFix API - removing deprecated functionImprove dependencies - removing deprecated functionRevert styles - handling edge casesChange dependencies - refactoring codeRemove UI - updating docs
